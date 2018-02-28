0b07cfe0f468314edab6043f31edd99930b195e5
Dominik Kaczmarek <magiczny.dominik.k@gmail.com>
2018-02-28 18:32:40 +0100

    init

---
+++ gitgudmain.py
@@ -0,0 +1,19 @@
+"""hash code 2018
+
+git gud production ;)
+"""
+
+def save_file(*args):
+    pass  # TODO
+
+
+def open_file(*args):
+    pass  # TODO
+
+
+def main():
+    print('hello # code')
+
+
+if __name__ == '__main__':
+    main()
