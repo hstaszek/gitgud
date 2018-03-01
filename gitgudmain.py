e95c44aab56be6759b334cacf4acf0a77d9edce9
jStefko <jakub.stefko@gmail.com>
2018-03-01 19:36:06 +0100

    Dijkstra Alghoritm

--- gitgudmain.py
+++ gitgudmain.py
@@ -3,6 +3,35 @@
 git gud production ;)
 """
 
+def dijsktra(graph, initial):
+  visited = {initial: 0}
+  path = {}
+
+  nodes = set(graph.nodes)
+
+  while nodes:
+    min_node = None
+    for node in nodes:
+      if node in visited:
+        if min_node is None:
+          min_node = node
+        elif visited[node] < visited[min_node]:
+          min_node = node
+
+    if min_node is None:
+      break
+
+    nodes.remove(min_node)
+    current_weight = visited[min_node]
+
+    for edge in graph.edges[min_node]:
+      weight = current_weight + graph.distance[(min_node, edge)]
+      if edge not in visited or weight < visited[edge]:
+        visited[edge] = weight
+        path[edge] = min_node
+
+  return visited, path
+
 def save_file(*args):
     pass  # TODO
 
