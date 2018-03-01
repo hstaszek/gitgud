41fec4d383ebea3be82e581239fe97c0664539f5
Dominik Kaczmarek <magiczny.dominik.k@gmail.com>
2018-03-01 19:12:40 +0100

    ininigttotot

--- gitgudmain.py
+++ gitgudmain.py
@@ -3,17 +3,33 @@
 git gud production ;)
 """
 
+import numpy as np
 
 def save_file(*args):
     pass  # TODO
 
 
-def open_file(*args):
-    pass  # TODO
+def open_file(filename):
+    with open(filename) as file:
+        warehoouse_list = file.readline().split()
+        print(warehoouse_list)
+
+        city = np.zeros([])
+
+        return Warehouse()
+
+class Warehouse:
+    def __init__(self, cityx, cityy, vehicle, rides, bonus, steps):
+        self.vehicles = vehicle
+        self.rides = rides
+        self.bonus = bonus
+        self.steps = steps
+
 
 
 def main():
     print('hello # code')
+    open_file("input/a_example.in")
 
 
 if __name__ == '__main__':
