83d7577d77ff87e9164051ea84c231c8897bd035
Dominik Kaczmarek <magiczny.dominik.k@gmail.com>
2018-03-01 19:30:35 +0100

    ininigttototfdsf121

--- gitgudmain.py
+++ gitgudmain.py
@@ -11,21 +11,40 @@ def save_file(*args):
 
 def open_file(filename):
     with open(filename) as file:
-        warehoouse_list = file.readline().split()
-        print(warehoouse_list)
+        w_list = file.readline().split()
+        print(w_list)
 
-        city = np.zeros([])
+        city = np.zeros([int(w_list[0]), int(w_list[1])])
 
-        return Warehouse()
+        return Warehouse(
+            int(w_list[0]),
+            int(w_list[1]),
+            int(w_list[2]),
+            int(w_list[3]),
+            int(w_list[4]),
+            int(w_list[5])
+        )
 
 class Warehouse:
-    def __init__(self, cityx, cityy, vehicle, rides, bonus, steps):
+    def __init__(self, x, y, vehicle, rides, bonus, steps):
         self.vehicles = vehicle
         self.rides = rides
         self.bonus = bonus
         self.steps = steps
+        self.city_x = x
+        self.city_y = y
+
+        self.city = np.zeros([x, y])
 
 
+class Auto:
+    def __init__(self, x, y, es, lf):
+        self.start_x = x
+        self.start_y = y
+        self.stan = 0
+        self.early_start = es
+        self.latest_finish = lf
+        self.points = []
 
 def main():
     print('hello # code')
