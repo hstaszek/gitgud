6a4438ed0dae5020ffc767b1e4d81fd3dc5bcf90
zaqs114 <zaqs114@gmail.com>
2018-03-01 19:48:19 +0100

    auta, warehouse

--- gitgudmain.py
+++ gitgudmain.py
@@ -5,6 +5,7 @@ git gud production ;)
 
 import numpy as np
 
+
 def save_file(*args):
     pass  # TODO
 
@@ -16,18 +17,26 @@ def open_file(filename):
 
         auto_list = []
         for line in file:
-            autoline = line.split()
-            auto_list.append(Auto(autoline[]))
-
+            al = line.split()
+            auto_list.append(
+                Auto(
+                    int(al[0]),
+                    int(al[1]),
+                    int(al[2]),
+                    int(al[3]),
+                    int(al[4]),
+                    int(al[5])
+                ))
 
         return Warehouse(
             int(w_list[0]),
             int(w_list[1]),
-            int(w_list[2]),
+            auto_list,
             int(w_list[3]),
             int(w_list[4]),
             int(w_list[5])
         )
+
 
 class Warehouse:
     def __init__(self, x, y, vehicle, rides, bonus, steps):
@@ -42,13 +51,14 @@ class Warehouse:
 
 
 class Auto:
-    def __init__(self, x, y,fx, fy,es, lf):
-        self.start =  [x, y]
+    def __init__(self, x, y, fx, fy, es, lf):
+        self.start = [x, y]
         self.finish = [fx, fy]
         self.stan = 0
         self.early_start = es
         self.latest_finish = lf
         self.points = []
+
 
 def main():
     print('hello # code')
